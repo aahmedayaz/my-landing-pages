27682ab904a7af98ecea4e88988a99f0ee515ef7
Ahmed Ayaz <ahmedayaz.business@gmail.com>
2022-09-13 12:39:56 +0500

    Scroll Added

--- Exercise 01 - Simple Landing Page/script.js
+++ Exercise 01 - Simple Landing Page/script.js
@@ -1,7 +1,7 @@
 let scrollDown = document.getElementById('see-more');
 scrollDown.addEventListener('click' , (e) => {
     window.scroll({
-        top:120,
+        top:590,
         behavior:'smooth'
     })
 })
